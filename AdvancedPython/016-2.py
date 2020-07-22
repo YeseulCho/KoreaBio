6ddde8701c7a5682b8e88b42e2ae80f9470e1ed1
YeseulCho <choys0313@naver.com>
2020-07-22 23:33:15 +0900

    add key:value in empty dictionary

--- AdvancedPython/016-2.py
+++ AdvancedPython/016-2.py
@@ -7,13 +7,13 @@ d = {}
 
 with open(f, 'r') as handle :
     for line in handle :
-        if line.startswith(">") :
+        if line.startswith(">") : # > 로 시작하는 행 제거
             continue
-        for s in line.strip() :
+        for s in line.strip() : # line에 존재하는 \n strip하고파일 전체  한글자씩 가져오기
             if s in d :
                 d[s] += 1
-            else :
-                d[s] = 1
+            else :              # d 안에 존재하지 않으면 1이라 지정
+                d[s] = 1        # 이렇게 지정해줬기에 그다음 A가 나오면 if문으로 넘어가 +1을 해줄수 있음 
 
 print(d)
 
